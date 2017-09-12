8aa899f9df532201ad9d0c32f53973a3f9af36a5
Jacques <jacqueslenormand@gmail.com>
2017-09-12 14:04:11 -0400

    initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+    public CubeController right;
+    public CubeController middle;
+    enum Position  {left, middle, right };
+    Position loc = Position.right;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = gameObject.transform.position;
+        pos.z = pos.z + 0.1f;
+        gameObject.transform.position = pos;
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(loc == Position.right)
+            {
+                loc = Position.middle;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (loc == Position.middle)
+            {
+                loc = Position.right;
+            }
+        }
+
+
+
+        if (loc != Position.right)
+        {
+            right.disable();
+        }
+        if (loc != Position.middle)
+        {
+            middle.disable();
+        }
+
+     
+        if (loc == Position.right)
+        {
+            right.enable();
+        }
+        if (loc == Position.middle)
+        {
+            middle.enable();
+        }
+
+    }
+}
---
+++ Assets/CubeController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CubeController : MonoBehaviour {
+    
+    public void disable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void enable()
+    {
+        gameObject.SetActive(true);
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.AddForce(Vector3.up * 300);
+            rb.useGravity = true;
+        }
+        if(gameObject.transform.position.y < 0)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.useGravity = false;
+            Vector3 pos = gameObject.transform.position;
+            pos.y = 0;
+            gameObject.transform.position = pos;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+}
